5d3870546e5a46f84ed413058468a2205475becb
minhhiennn <18130076@st.hcmuaf.edu.vn>
2021-07-17 21:57:20 +0700

    them ti

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { TaskbarComponent } from './Component/taskbar/taskbar.component';
-import { ChatComponent } from './Component/chat/chat.component';
-import { ChatMessengerComponent } from './Component/taskbar/chat-messenger/chat-messenger.component';
 
-<<<<<<< HEAD
-const routes: Routes = [
-  { path: "", component: TaskbarComponent },
-  { path: "ChatComponent", component: ChatComponent },
-  { path: "ChatMessengerComponent", component: ChatMessengerComponent },
-];
-=======
 import { ChatComponent } from './Component/chat/chat.component';
 import { FriendlistComponent } from './Component/friendlist/friendlist.component';
 import { HomeComponent } from './Component/home/home.component';
@@ -24,13 +14,12 @@ const routes: Routes = [
   { path: '', component: ChatComponent }
 ];
 
->>>>>>> 4f134b0c3ce1e05409b74e233c30dab7a08df0ad
 @NgModule({
   declarations: [
     ChatComponent,
     HomeComponent,
     FriendlistComponent,
-    SettingComponent,
+    SettingComponent
   ],
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { TaskbarComponent } from './Component/taskbar/taskbar.component';
-<<<<<<< HEAD
-import { ChatComponent } from './Component/chat/chat.component';
-import { ChatMessengerComponent } from './Component/taskbar/chat-messenger/chat-messenger.component';
-=======
->>>>>>> 4f134b0c3ce1e05409b74e233c30dab7a08df0ad
+
 
 @NgModule({
   declarations: [
     AppComponent,
     TaskbarComponent,
-<<<<<<< HEAD
-    ChatComponent,
-    ChatMessengerComponent,
-=======
->>>>>>> 4f134b0c3ce1e05409b74e233c30dab7a08df0ad
   ],
   imports: [
     BrowserModule,
