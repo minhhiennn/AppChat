4f134b0c3ce1e05409b74e233c30dab7a08df0ad
minhhiennn <18130076@st.hcmuaf.edu.vn>
2021-07-17 11:52:59 +0700

    them ti

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { ChatComponent } from './Component/chat/chat.component';
+import { FriendlistComponent } from './Component/friendlist/friendlist.component';
+import { HomeComponent } from './Component/home/home.component';
+import { SettingComponent } from './Component/setting/setting.component';
+
+const routes: Routes = [
+  { path: 'chat', component: ChatComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'friendlist', component: FriendlistComponent },
+  { path: 'setting', component: SettingComponent },
+  { path: '', component: ChatComponent }
+];
 
 @NgModule({
+  declarations: [
+    ChatComponent,
+    HomeComponent,
+    FriendlistComponent,
+    SettingComponent,
+  ],
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TaskbarComponent } from './Component/taskbar/taskbar.component';
-import { ChatComponent } from './Component/chat/chat.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TaskbarComponent,
-    ChatComponent,
   ],
   imports: [
     BrowserModule,
